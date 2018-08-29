de1138f4ff584fa7c401027ad5a8ad6b459e76be
PAULO VINICIUS DA SILVA SOARES 114110478 <paulovss@lcc3-023.lcc.ufcg.edu.br>
2018-08-29 17:37:25 -0300

    Conecta mongoose e adiciona bodyParser

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 // Inicializa Express
 const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 // Importa rotas
 const usuarios = require("./routes/api/usuarios");
@@ -7,6 +9,22 @@ const alunos = require("./routes/api/usuarios");
 const disciplinas = require("./routes/api/usuarios");
 
 const app = express();
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// DB Config
+const db = require("./config/keys").mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("Banco de dados conectado!"))
+  .catch(err => console.log(err));
 
 // Chama rotas na aplicação
 app.use("/api/usuarios", usuarios);
