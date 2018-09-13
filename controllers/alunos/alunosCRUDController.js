9759f027d66938202234b85ee64952a71bdb3d6d
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2018-09-13 00:42:36 -0300

    CRUD de alunos integrado com as rotas

--- controllers/alunos/alunosCRUDController.js
+++ controllers/alunos/alunosCRUDController.js
@@ -1,16 +1,26 @@
 const { Aluno } = require("../../sequelize");
 
 module.exports = {
-  create(req, res) {
-    Aluno.create({ "matricula": req.body.matricula, "nome": req.body.nome }).then(aluno => {
-      aluno.UsuarioMatricula = req.body.matricula;
-      aluno.save();
+  cadastraOuAtualiza(req, res) {
+    Aluno.findOrCreate({ where: { matricula: req.body.matricula }, defaults: { nome: req.body.nome, UsuarioMatricula: req.body.matricula } }).spread((aluno, created) => {
+      if (created) res.json(aluno);
+      else {
+        aluno.matricula = req.body.matricula;
+        aluno.nome = req.body.nome;
+        aluno.UsuarioMatricula = req.body.matricula;
+        aluno.save();
 
-      return (res.json(aluno));
+        res.json(aluno);
+      }
     });
   },
-  getAll() { },
-  getById() { },
-  update() { },
-  deleteById() { }
+  verTodos(req, res) {
+    Aluno.findAll().then(alunos => res.json(alunos));
+  },
+  verAluno(req, res) {
+    Aluno.findById(req.params.matricula).then(aluno => res.json(aluno));
+  },
+  apagaAluno(req, res) {
+    Aluno.destroy({ where: { matricula: req.params.matricula } }).then(opStatus => opStatus === 1 ? res.status(200).json({ "msg": "Success" }) : res.status(400).json({ "msg": "Usuário não existe" }));
+  }
 };
--- routes/api/alunos.js
+++ routes/api/alunos.js
@@ -3,41 +3,26 @@ const logger = require("heroku-logger");
 
 const router = express.Router();
 
-const { create } = require("../../controllers/alunos/alunosCRUDController");
-
-// @route   POST api/alunos/
-// @desc    Cria um aluno
-// @access  Public
-// router.post("/", (req, res) => {
-//   logger.info(req.body);
-//   Aluno.create(req.body);
-
-//   const disciplina = req.body.disciplinas[0];
-
-//   Disciplina.create(disciplina).then(disciplina => {
-//     disciplina.AlunoMatricula = req.body.matricula;
-//     disciplina.save();
-//   });
-// });
-
-// @route   POST api/alunos/
-// @desc    Cria um aluno
-// @access  Public
-router.post("/", create);
+const { cadastraOuAtualiza, verTodos, verAluno, apagaAluno } = require("../../controllers/alunos/alunosCRUDController");
 
 // @route   GET api/alunos/
 // @desc    Pega lista de alunos cadastrados
 // @access  Public
-router.get("/", (req, res) => {
-  logger.info(req.body);
-  Aluno.findAll().then(alunos => res.json(alunos));
-});
+router.get("/", verTodos);
 
-// @route   GET api/alunos/test
-// @desc    Testa a rota de alunos
+// @route   POST api/alunos/
+// @desc    Cria um aluno ou atualiza seus dados pessoais
 // @access  Public
-router.get("/test", (req, res) =>
-  res.json({ msg: "Testando a rota de alunos." })
-);
+router.post("/", cadastraOuAtualiza);
+
+// @route   GET api/alunos/:matricula
+// @desc    Pega lista de alunos cadastrados
+// @access  Public
+router.get("/:matricula", verAluno);
+
+// @route   DELETE api/alunos/:matricula
+// @desc    Apaga um aluno
+// @access  Public
+router.delete("/:matricula", apagaAluno);
 
 module.exports = router;
