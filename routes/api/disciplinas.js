29c238d065a033aa98148b0b0f449ca0020e3238
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2018-09-13 01:04:12 -0300

    Cria ações na controller e instancia nas rotas de disciplina

--- routes/api/disciplinas.js
+++ routes/api/disciplinas.js
@@ -1,11 +1,26 @@
 const express = require("express");
 const router = express.Router();
 
-// @route   GET api/profile/test
-// @desc    Testa a rota de perfil
+const { cadastraOuAtualizaDisciplina, verTodas, verDisciplina, apagaDisciplina } = require("../../controllers/disciplinas/disciplinasCRUDController");
+
+// @route   GET api/disciplinas/
+// @desc    Pega lista de disciplinas cadastradas
 // @access  Public
-router.get("/test", (req, res) =>
-  res.json({ msg: "Testando a rota de perfil." })
-);
+router.get("/", verTodas);
+
+// @route   POST api/disciplinas/
+// @desc    Cria um disciplina ou atualiza seus dados
+// @access  Public
+router.post("/", cadastraOuAtualizaDisciplina);
+
+// @route   GET api/disciplinas/:codigo
+// @desc    Pega disciplina pelo seu código
+// @access  Public
+router.get("/:codigo", verDisciplina);
+
+// @route   DELETE api/disciplinas/:codigo
+// @desc    Apaga uma disciplina
+// @access  Public
+router.delete("/:matricula", apagaDisciplina);
 
 module.exports = router;
