9c1de0e5c4e5575fe9988667560172b7e05244b6
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2018-10-01 17:47:40 -0300

    Altera cadastro de usuários usando mongoose

--- controllers/usuarios/usuariosController.js
+++ controllers/usuarios/usuariosController.js
@@ -1,9 +1,44 @@
-const { Usuario } = require("../../sequelize");
+const Usuario = require("../../models/Usuario");
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const keys = require("../../config/keys");
+const passport = require("passport");
 
 module.exports = {
   cadastra(req, res) {
-    Usuario.create({ "matricula": req.body.matricula, "senha": req.body.senha, "email": req.body.email }).then(usuario => res.json(usuario));
+    Usuario.findOne({ email: req.body.email }).then(user => {
+      if (user) {
+        return res.status(400).json({ "errors.email": "Email já cadastrado!" });
+      } else {
+        const avatar = gravatar.url(req.body.email, {
+          s: "200", // Size
+          r: "pg", // Rating
+          d: "mm" // Default
+        });
+
+        // Create User
+        const newUser = new User({
+          nome: req.body.nome,
+          email: req.body.email,
+          avatar,
+          senha: req.body.senha
+        });
+
+        // Encrypt password
+        bcrypt.genSalt(SALT_SIZE, (err, salt) => {
+          bcrypt.hash(newUser.senha, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.senha = hash;
+            newUser
+              .save()
+              .then(user => res.json(user))
+              .catch(err => console.log(err));
+          });
+        });
+      }
+    });
   },
-  login(req, res) { },
-  getUsuarioAtual(req, res) { },
+  login(req, res) {},
+  getUsuarioAtual(req, res) {}
 };
