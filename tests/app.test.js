237f4e033be9d307425d6f0a3b31c00bfe329256
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2018-10-08 00:59:20 -0300

    Adiciona teste para registro de usuário

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,11 +1,26 @@
-const app = require("../server");
 const request = require("supertest");
+const app = require("../server");
 
-request(app)
-  .get("/user")
-  .expect("Content-Type", /json/)
-  .expect("Content-Length", "15")
-  .expect(200)
-  .end(function(err, res) {
-    if (err) throw err;
+/**
+ * Testing post user endpoint
+ */
+describe('POST /api/usuarios', function () {
+  let data = {
+    "nome": "Paulo Soares",
+    "email": "paulo.soares@ccc.ufcg.edu.br",
+    "matricula": "114110999",
+    "senha": "shakepaulinha"
+  }
+  it('respond with 200 OK', function (done) {
+    request(app)
+      .post('/api/usuarios')
+      .send(data)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end((err) => {
+        if (err) return done(err);
+        done();
+      });
   });
+});
