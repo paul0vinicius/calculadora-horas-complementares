c4d57f80bae08627afd4999689432f1473507104
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2018-10-09 18:12:34 -0300

    Adiciona comentário em disciplinas

--- controllers/disciplinas/disciplinasCRUDController.js
+++ controllers/disciplinas/disciplinasCRUDController.js
@@ -1,3 +1,5 @@
+// As disciplinas serão adicionadas via API do MLAB, porém para exercitar alguns conceitos
+// vou adicionar a verificação do role. Teoricamente só um admin poderia fazer o CRUD das disciplinas.
 const Disciplina = require("../../models/Disciplina");
 
 module.exports = {
